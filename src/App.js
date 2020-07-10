da9271a7df32b1d2b71488fb139c375a81c401cd
Namwanza <namwanzaronald4@gmail.com>
2020-07-10 09:32:21 +0300

    Setting ClassNames Dynamically

--- src/App.js
+++ src/App.js
@@ -1,32 +1,92 @@
 import React, { Component } from 'react';
-import './App.css';
 import Person from './Person/Person'
+import './App.css';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
-    name: "Skyla"
+    persons: [
+      {id: 'hsisi', name: '', age: 27},
+      {id: 'eslkd', name: '', age: 26},
+      {id: 'ossk', name: '', age: 1},
+    ], 
+    userInputs: ''
   }
 
-  nameChangedHandler = () => {
+  nameChangedHandler = (event, id) => {
+    const PersonIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+
+    const y = {
+      ...this.state.persons[PersonIndex]
+    }
+
+    y.name = event.target.value;
+    const x = [...this.state.persons];
+    x[PersonIndex] = y;
     this.setState({
-      name: "Vanpretty"
+      persons: x
     })
   }
 
+  deletePersonHandler = (index) => {
+    const PersonIndex = [...this.state.persons];
+    PersonIndex.splice(index, 1);
+    this.setState({
+      persons: PersonIndex
+    })
+  }
+
+  // Adding two binding
+  nameHandler = (newName) => {
+    this.setState({
+      persons: [
+        {name: newName, age: 27},
+        {name: 'Ronnie', age: 25},
+        {name: 'Namwanza', age: 20},
+      ], 
+    })
+  }
+
+
   render () {
+    const Persons = this.state.persons.map(person => {
+      return (
+       <Person 
+         name = {person.name} 
+         age = {person.age}
+         key = {person.id}
+         changed = {(event) => this.nameChangedHandler(event, person.id)}
+         deleted = {() => this.deletePersonHandler(person.id)}
+       />
+      )
+    })
+
+    // Dynamically changing classNames
+    let Button = [];
+    this.state.persons.map(p => {
+      return (
+        !p.name ? Button.push('Button-style') : Button.push('Button-style-02')
+      )
+    })
+    
+    
     return (
       <div className="App">
-        <span> {this.state.name} </span>
-        <button onClick = {this.nameChangedHandler} >switch Names</button>
-        <Person 
-          assignment = "Computer Science" 
-          homework = "Arithematic operators"
+        <h4>Setting ClassNames Dynamically</h4>
+        <button style={{
+          padding: '10px', 
+          margin: '10px'}}
+          onClick = {() => this.nameHandler('Vanpretty')}
+          className = {Button.join(' ')}
         >
-          Hey am a child
-        </Person>
+          Name Changed
+        </button>
+        {Persons}
       </div>
     );
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import Student from '../Student/Student'
+import './Person.css';
+import Radium from 'radium';
 
 const Person = (props) => {
+    const styles = [];
+    if (props.name.length >= 3 && props.name.length < 6) {
+        styles.push('Input-01')
+    } else if (props.name.length >= 6 && props.name.length < 10) { 
+        styles.push('Input-02')
+    } else if (props.name.length >= 10) {
+        styles.push('Input-03')
+    } else {
+        styles.push('')
+    }
     return (
-      <div>
-         <h1>Hey I'm a Component Person</h1>
-         <span>{props.assignment}</span> /
-         <span> {props.homework} </span>/
-         <span> {props.children} </span>
-         <Student age = {24} />
-      </div>
+        <div>
+            <p onClick = {props.deleted}> My name is: {props.name} and I'm {props.age} years old </p>
+            <input 
+                
+                type = "text"
+                value = {props.name}
+                onChange = {props.changed}
+             />
+        </div>
     )
 }
-export default Person;
+
+export default Radium(Person);
