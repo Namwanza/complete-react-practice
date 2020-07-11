08a4a1fbfb34542ed5d3e94d96db72ca66371ff3
Namwanza <namwanzaronald4@gmail.com>
2020-07-11 13:14:38 +0300

    Manipulating states using React Hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import styled from 'styled-components';
 
-class App extends Component {
-  state = {
-    name: "Skyla"
+const Button = styled.button `
+  padding: 10px;
+  background-color: ${props => props.alt = 'green'};
+  border-style: none;
+  color: white;
+  margin: 10px;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: ${props => props.alt = 'red'}
+  }
+`;
+
+const App = () => {
+  const [name, stateChanged] = useState(['Roger', 'Willy', 'Ronnie']);
+  const [setButton, buttonChanged] = useState(false)
+
+  const nameChangedHandler = (event, index) => {
+    const displayPerson = [...name];
+    // console.log(displayPerson)
+    displayPerson[index] = event.target.value
+    stateChanged(displayPerson)
   }
 
-  nameChangedHandler = () => {
-    this.setState({
-      name: "Vanpretty"
-    })
+  const toggleButton = () => {
+    buttonChanged(!setButton)
   }
 
-  render () {
-    return (
-      <div className="App">
-        <span> {this.state.name} </span>
-        <button onClick = {this.nameChangedHandler} >switch Names</button>
+  let showPerson = null
+  if (!setButton) {
+    showPerson = <div>
+      <div>{name.join(', ')}</div>
+      {
+        name.map((p, id) => 
         <Person 
-          assignment = "Computer Science" 
-          homework = "Arithematic operators"
-        >
-          Hey am a child
-        </Person>
-      </div>
-    );
+          name = {p} 
+          key = {id}
+          alt = {p}
+          change = {(event) => nameChangedHandler(event, id)}
+        />
+      )}
+    </div>
+  } else {
+    showPerson = null;
   }
+
+  return (
+    <div className="App">
+      <Button onClick={toggleButton}>switch Names</Button>
+      {showPerson}
+    </div>
+  );
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Student from '../Student/Student'
 
 const Person = (props) => {
     return (
       <div>
-         <h1>Hey I'm a Component Person</h1>
-         <span>{props.assignment}</span> /
-         <span> {props.homework} </span>/
-         <span> {props.children} </span>
-         <Student age = {24} />
+        <input 
+            type = "text" 
+            value = {props.name} 
+            onChange = {props.change}
+        />
       </div>
     )
 }
